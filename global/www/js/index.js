98423b627543844563d9d4ed46cc3e02f4d27681
Kirk Elliott <kirk@addthis.com>
2016-04-10 14:45:02 -0400

    added android app, ui by language

--- global/www/js/index.js
+++ global/www/js/index.js
@@ -16,36 +16,46 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var config = require('./app/config');
 
-        console.log('Received Event: ' + id);
-    }
-};
+module.exports = (function () {
+		document.addEventListener('deviceready', appReady, false);
 
-app.initialize();
+		function appReady() {
+			// this setTimeout is to allow a developer six seconds
+			// to connect a debugger before the app initializes
+			// should be commented out for release, but uncomment appInit()
+			//setTimeout(function () {
+				appInit();
+			//}, 6000)
+		}
+
+		function appInit () {
+			$(function () {
+				if (config.track && analytics) {
+					analytics.startTrackerWithId(config.trackId);
+					analytics.trackEvent('Init', 'Load', 'App Started', 10);
+				}
+				navigator.globalization.getPreferredLanguage(
+					function (language) {
+						analytics.trackEvent('Language', 'Load', language.value, 10);
+						if (language && language.value) {
+							if (language.value.indexOf("ar") > -1) {
+								$('body').addClass('arabic-ui');
+							} else if (language.value.indexOf("ru") > -1) {
+								$('body').addClass('russian-ui');
+							} else if (language.value.indexOf("zh") > -1) {
+								$('body').addClass('chinese-ui');
+							}
+						}
+						require('./init');
+					},
+					function () {
+						analytics.trackEvent('Language', 'Fail', language.value, 10);
+						require('./init');
+					}
+				);
+			});
+		}
+}());
